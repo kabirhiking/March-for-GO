5a545578a870965d5028ec4541a22964b4d4b2e7
kabir <kabirraihan249@gmail.com>
2025-04-22 23:20:35 +0600

    standard functtion or named function

---
+++ FUNCTION_TYPE/standard_or_name.go
@@ -0,0 +1,15 @@
+//standard or named function
+package main
+import "fmt"
+
+func add(x, y int){
+	fmt.Println(x + y)
+}
+
+
+func main(){
+
+
+	add(6, 9)
+}
+//standard function is a function that is defined using the standard function syntax in Go.
