653f094d95b5d3dd5f88add996ce611a55a3b3e8
kabir <kabirraihan249@gmail.com>
2025-04-30 18:48:41 +0600

    array

---
+++ ARRAY/array.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+var arr2 = [3]string{"I", "Love", "You"}
+
+func main() {
+	// var arr [2] int
+	// arr[1] = 6
+	arr := [2]int{3,5}
+
+	fmt.Println(arr)
+	fmt.Println(arr2[2])
+
+}
